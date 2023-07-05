324c2a17435d26e7d824262360bc07cf18bceeb1
AYAZRIAZ-006 <117469361+AYAZRIAZ-006@users.noreply.github.com>
2023-07-05 15:04:39 +0500

    add update field api

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,10 +1,12 @@
 import SignUp from "./signup.js";
 import SignIn from "./signin.js";
 import insertMany from "./insertmany.js";
+import updateField from "./updateField.js";
 // import ForgetPasswordEmail from "./forget-password-email.js";
 export {
     SignUp,
     SignIn,
     insertMany,
+    updateField,
     // ForgetPasswordEmail,
 };
